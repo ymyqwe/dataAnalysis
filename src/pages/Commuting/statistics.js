9f178c970a9e70e1458b559057ce638d6959a06f
sylvia <sylvia@manias-Mac-mini.local>
2020-05-26 23:15:41 +0800

    :sparkles: 完成出行图表

--- src/pages/Commuting/statistics.js
+++ src/pages/Commuting/statistics.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { Chart } from '@antv/g2';
 import data from '../../data/commuting/handledTrainData.json';
+import { min } from 'moment';
 
 class Home extends Component {
   componentDidMount() {
     const chart = new Chart({
       container: 'container',
-      autoFit: true,
+      // autoFit: true,
       height: 500,
+      width: 800,
+      // padding: [20],
     });
 
     chart.data(data);
@@ -16,19 +19,25 @@ class Home extends Component {
         alias: '日期',
         type: 'time',
       },
-      start_time: {
+      minutes: {
         alias: '出发时间',
-        min: 0,
         sync: true, // 将 pv 字段数值同 time 字段数值进行同步
         nice: true,
+        ticks: [500, 520, 540, 560, 580, 600],
+        formatter: (text, item) => {
+          const hour = Math.floor(Number(text) / 60);
+          const minutes = Number(text) % 60;
+          console.log('textt', text);
+          return `${hour < 10 ? '0' + hour : hour}:${minutes === 0 ? '00' : minutes}`;
+        },
       },
-      // count: {
-      //   alias: '次数',
-      // },
+      count: {
+        alias: '次数',
+      },
     });
 
-    chart.axis('start_time', {
-      title: {},
+    chart.axis('minutes', {
+      label: {},
     });
 
     chart.tooltip({
@@ -39,7 +48,7 @@ class Home extends Component {
     chart
       .line()
       .position('start_date*minutes')
-      .color('status_name');
+      .color('status_name', ['#1890ff', '#ced4d9']);
 
     chart
       .point()
@@ -59,7 +68,7 @@ class Home extends Component {
     return (
       <div>
         <div>折线图</div>
-        <div id="container" />
+        <div id="container" style={{ padding: '50px' }} />
       </div>
     );
   }
